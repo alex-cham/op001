bdc7c57a1e3601bdb2c2dd36822be0d56f44c134
alex-cham <34160079+alex-cham@users.noreply.github.com>
2021-09-30 17:17:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git openwrt-packages https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
+git clone https://github.com/kenzok8/openwrt-packages package/openwrt-packages
